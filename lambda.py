87652bc80ffe43504eb7acd1430ebc7dbbf98eb2
aravindmurugesh15 <126728673+aravindmurugesh15@users.noreply.github.com>
2023-03-02 12:12:03 -0600

    Update lambda.py

--- lambda.py
+++ lambda.py
@@ -9,16 +9,16 @@ def lambda_handler(event, context):
    
    client = boto3.client('secretsmanager')
    cjamsdb = client.get_secret_value(SecretId= os.environ.get('db_cred'))
-   # s3_path = client.get_secret_value(SecretId='cjams_s3_path')
+   # s3_path = client.get_secret_value(SecretId='s3_path')
 
-   dn_cred = json.loads(cjamsdb['SecretString'])
+   dn_cred = json.loads(db['SecretString'])
    # s3_loc = json.loads(s3_path['SecretString'])
 
 #establishing the connection
    conn = psycopg2.connect(
           database=dn_cred['dbname'],
-          user=dn_cred['username_cjams'],
-          password=dn_cred['password_cjams'],
+          user=dn_cred['username'],
+          password=dn_cred['password'],
           host=dn_cred['host'],
           port=dn_cred['port']
          )
@@ -27,7 +27,7 @@ def lambda_handler(event, context):
    #sql_query = """select user,usename,query,calls,avgtime_seconds,state from (select  user,usename,query,calls,avgtime_seconds,state,RANK () OVER (ORDER BY avgtime_seconds desc) as rank_time from(SELECT  user,  usename,  query,  state,  count(query) as calls,  avg(state_change - query_start) as avgtime_seconds FROM pg_stat_activity where query !='' and datname ='mdtcjamsdbs' and state in ('idle','active') and (query_start > (now() - interval '30 minutes')) group by user,usename,query,state order by avgtime_seconds desc) as pg_stat) as final where final.rank_time <= 20"""
    sql_query_lr = """select usename,query,state,calls,avgtime_seconds from (select  user,usename,query,state,calls,avgtime_seconds,RANK () OVER (ORDER BY avgtime_seconds desc) as rank_time from(   SELECT  datname,user,  usename,  query,  state,  count(query) as calls, avg(EXTRACT(EPOCH FROM (current_timestamp - query_start))) as avgtime_seconds FROM pg_stat_activity where query !=''  and state in ('active') and datname='mdtcjamsdbs' and (query not like 'SET%' and query not like 'SHOW%' and query not like '%pg_%' and query not like 'START_REPLICATION%') group by datname,user,usename,query,state order by avgtime_seconds desc) as pg_stat) as final where final.usename not like 'rds%' and final.avgtime_seconds>=5 order by avgtime_seconds desc"""
 
-   sql_query_conn = """with t1 as (select count(*) as total_count from pg_stat_activity where state in ('idle','active') and usename not like '%rds%') select usename,state,client_addr,count(*) as count,t1.total_count FROM pg_stat_activity a,t1 where 1=1 and state in ('idle','active') and usename not like '%rds%' group by usename,state,t1.total_count,client_addr  order by count desc"""
+   sql_query_conn = """with t1 as (select count(*) as total_count from pg_stat_activity where state in ('idle','active') and usename not like '%rds%') select usename,state,client_addr,count(*) as db_connectioncount_peruser,t1.total_count as db_total_count FROM pg_stat_activity a,t1 where 1=1 and state in ('idle','active') and usename not like '%rds%' group by usename,state,db_total_count,client_addr  order by db_connectioncount_peruser desc"""
         
 #Creating a cursor object using the cursor() method
    cursor = conn.cursor()
@@ -52,14 +52,14 @@ def lambda_handler(event, context):
    
    data1 = cursor.fetchall()
    
-   df1 = pd.DataFrame(data1,columns=['usename','state','client_addr','count','total_count'])
+   df1 = pd.DataFrame(data1,columns=['usename','state','client_addr','db_connectioncount_peruser','db_total_count'])
    
    table = df.to_html(header="true", table_id="table").replace('<th>', '<th align="left">')
    
    table1 = df1.to_html(header="true", table_id="table").replace('<th>', '<th align="left">')
    
-   #html = '<html> <body> <h2>CJAMS Top 20 Long Running Queries</h2> <table>' + table + '</table> </body> </html>' + '<html> <body> <h2>CJAMS Prod Connections</h2> <table>' + table1 + '</table> </body> </html>'
-   html = '<html> <body> <h2>CJAMS Top 20 Long Running Queries (>5 Secs)</h2>' + table + '<h2>CJAMS Prod Connections</h2>' + table1 + '</body> </html>'
+   #html = '<html> <body> <h2>Top 20 Long Running Queries</h2> <table>' + table + '</table> </body> </html>' + '<html> <body> <h2>Prod Connections</h2> <table>' + table1 + '</table> </body> </html>'
+   html = '<html> <body> <h2>Top 20 Long Running Queries (>5 Secs)</h2>' + table + '<h2>Prod Connections</h2>' + table1 + '</body> </html>'
    
    print (html)
    
